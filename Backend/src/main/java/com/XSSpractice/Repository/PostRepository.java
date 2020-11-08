196b6749363b5de176279fd5edacaba7a88b6be3
vividswan <vividswan@naver.com>
2020-11-08 20:26:00 +0900

    Update PostRepository.java

--- Backend/src/main/java/com/XSSpractice/Repository/PostRepository.java
+++ Backend/src/main/java/com/XSSpractice/Repository/PostRepository.java
@@ -1,10 +1,7 @@
 package com.XSSpractice.Repository;
 
 import com.XSSpractice.model.Post;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface PostRepository extends JpaRepository<Long, Post> {
-    public Page<Post> findAllPost(Pageable pageable);
+public interface PostRepository extends JpaRepository<Post, Long> {
 }
