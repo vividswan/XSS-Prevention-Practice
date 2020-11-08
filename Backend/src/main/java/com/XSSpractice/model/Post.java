48c9df4ef8c8d00e278eaa01797dd1908eb29f7e
vividswan <vividswan@naver.com>
2020-11-08 20:25:54 +0900

    Update Post.java

--- Backend/src/main/java/com/XSSpractice/model/Post.java
+++ Backend/src/main/java/com/XSSpractice/model/Post.java
@@ -1,16 +1,26 @@
 package com.XSSpractice.model;
 
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.apache.tomcat.jni.Local;
 import org.springframework.data.annotation.CreatedDate;
 
 import javax.persistence.*;
 import java.time.LocalDateTime;
 
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
 @Entity
 public class Post {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
+    private String title;
 
     @Lob
     private String content;
@@ -18,5 +28,10 @@ public class Post {
     @CreatedDate
     private LocalDateTime createdTime;
 
-    private String User;
+    private String user;
+
+    public void update(String title, String content){
+        this.title = title;
+        this.content = content;
+    }
 }
